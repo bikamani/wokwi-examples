95f6caf38fa5370919dae7dc4d65c9b52fb18620
bikamani <kamanibinal@gmail.com>
2023-12-03 16:22:17 -0800

    add modified sample with push button and led

---
+++ mqtt-sample-modified.py
@@ -0,0 +1,72 @@
+"""
+Modified IoT Weather Station Example with triggering update of weather using a push button.
+
+To view the data:
+
+1. Go to http://www.hivemq.com/demos/websocket-client/
+2. Click "Connect"
+3. Under Subscriptions, click "Add New Topic Subscription"
+4. In the Topic field, type "wokwi-weather" then click "Subscribe"
+
+Now click on the push button in the simulation,
+current temperature, humidity will be reported,
+and you should see the message appear on the MQTT Broker,
+in the "Messages" pane.
+
+https://wokwi.com/arduino/projects/322577683855704658
+"""
+
+import network
+import time
+import machine
+from machine import Pin
+from utime import sleep
+import dht
+import ujson
+from umqtt.simple import MQTTClient
+
+# MQTT Server Parameters
+MQTT_CLIENT_ID = "micropython-weather-demo"
+MQTT_BROKER    = "broker.mqttdashboard.com"
+MQTT_USER      = ""
+MQTT_PASSWORD  = ""
+MQTT_TOPIC     = "wokwi-weather"
+
+sensor = dht.DHT22(Pin(15))
+
+led = Pin(14, Pin.OUT)
+button = machine.Pin(26, machine.Pin.IN, machine.Pin.PULL_UP)
+
+print("Connecting to WiFi", end="")
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect('Wokwi-GUEST', '')
+while not sta_if.isconnected():
+  print(".", end="")
+  time.sleep(0.1)
+print(" Connected!")
+
+print("Connecting to MQTT server... ", end="")
+client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, user=MQTT_USER, password=MQTT_PASSWORD)
+client.connect()
+
+print("Connected!")
+
+prev_weather = ""
+while True:
+  print("Measuring weather conditions... ", end="")
+  sensor.measure() 
+  message = ujson.dumps({
+    "temp": sensor.temperature(),
+    "humidity": sensor.humidity(),
+  })
+
+  if button.value() == 0:
+    print("Updated!")
+    print("Reporting to MQTT topic {}: {}".format(MQTT_TOPIC, message))
+    client.publish(MQTT_TOPIC, message)
+    led.value(1)
+    prev_weather = message
+  else:
+    led.value(0)
+  time.sleep(0.2)
